131bd74765c33aa81302dac1db018302aba6f838
fiore57 <fiore57@pc-gear.com>
2019-10-27 03:13:47 +0900

    Fix bug of JSON parse test

--- src/test/suite/json.test.ts
+++ src/test/suite/json.test.ts
@@ -4,15 +4,15 @@ import * as json from '../../json';
 
 
 suite('Extension Test Suite', () => {
-	const jsonSample1 = `
+	const jsonSample1: string = `
 // It is sample snippet!
-"Print to console": {	// this is /* snippet name */
+{"Print to console": {	// this is /* snippet name */
 "prefix" /* prefix means "trigger" */ :"log",
 	"body": [
-		"// output to console"
-		"console.log('$1');",
-		"\${2:/*here*/}"
-	],
+		"// output to console",
+/*====*/"console.log('$1');",`
+		+ '		"${2:/*here*/}",' +	// 「${x:...}」が「``」に入らないため、シングルクォート
+		`	],
 
 	"description": "Log output to console",	/* oops! */
 }
@@ -20,9 +20,10 @@ suite('Extension Test Suite', () => {
 /* COMMENT */   ///  comment  ///
 /***********/   /////////////////
 , // ???
+}
 `
 
-	test('JSON parse test', () => {
+	test('JSON parse test1', () => {
 		assert.notStrictEqual(json.parseJson(jsonSample1), undefined);
 	});
 });
